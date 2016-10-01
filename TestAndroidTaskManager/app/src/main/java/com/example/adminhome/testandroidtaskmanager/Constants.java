513ef345218a6a8b8c775f4cbc4ffb0c2f974a86
Capitalsav <capitalsav@gmail.com>
2016-10-01 19:14:23 +0300

    1. Created class Constants for all project. 2. Created class MySQLiteOpenHelper for work with SQLite.

---
+++ TestAndroidTaskManager/app/src/main/java/com/example/adminhome/testandroidtaskmanager/Constants.java
@@ -0,0 +1,9 @@
+package com.example.adminhome.testandroidtaskmanager;
+
+/**
+ * Created by AdminHome on 01.10.2016.
+ */
+public class Constants {
+
+    public static final String TAG = "=======LOG_TAG_DEBUG";
+}
